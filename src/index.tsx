3689741a8dd23ec80774c4c763c94190f32200c6
genspark_dev <genspark_dev@genspark.ai>
2026-04-27 05:46:28 +0000

    fix: 증발가스 보고서 측정실/측정장비 6열 균등 구조로 재조정 - 6개 입력칸 동일 비율 적용

--- src/index.tsx
+++ src/index.tsx
@@ -3626,32 +3626,30 @@ if (formType==='detail_plan') return (
   <!-- ══════════════════════════════════════════ -->
   <table class="ev-tbl" style="border-top:none;">
     <colgroup>
-      <col style="width:18%;"><!-- 라벨1 -->
-      <col style="width:10%;"><!-- 높이값 -->
-      <col style="width:10%;"><!-- 폭값 -->
-      <col style="width:10%;"><!-- 길이값 -->
-      <col style="width:18%;"><!-- 라벨2 -->
-      <col style="width:11%;"><!-- 값2 -->
-      <col style="width:12%;"><!-- 라벨3 -->
-      <col style="width:11%;"><!-- 값3 -->
+      <col style="width:20%;"><!-- 라벨A -->
+      <col style="width:13%;"><!-- 값A -->
+      <col style="width:17%;"><!-- 라벨B -->
+      <col style="width:13%;"><!-- 값B -->
+      <col style="width:17%;"><!-- 라벨C -->
+      <col style="width:20%;"><!-- 값C -->
     </colgroup>
     <tbody>
       <tr>
-        <th class="ev-sec-th" colspan="8">2. &nbsp;측정실 및 측정장비</th>
+        <th class="ev-sec-th" colspan="6">2. &nbsp;측정실 및 측정장비</th>
       </tr>
       <!-- 측정실(밀폐실) 규격 : 높이 / 폭 / 길이 / 순내부체적 -->
       <tr>
         <td class="ev-th" style="white-space:nowrap;">측정실(밀폐실) 규격 :</td>
         <td><span class="ev-lbl" style="white-space:nowrap;">높이 :</span><input data-field="ev_room_h" class="ev-inp" type="text" value="\${E(v('ev_room_h'))}"></td>
-        <td><span class="ev-lbl" style="white-space:nowrap;">폭 :</span><input data-field="ev_room_w" class="ev-inp" type="text" value="\${E(v('ev_room_w'))}"></td>
+        <td style="text-align:center;"><span class="ev-lbl" style="white-space:nowrap;">폭 :</span><input data-field="ev_room_w" class="ev-inp" type="text" value="\${E(v('ev_room_w'))}"></td>
         <td><span class="ev-lbl" style="white-space:nowrap;">길이 :</span><input data-field="ev_room_l" class="ev-inp" type="text" value="\${E(v('ev_room_l'))}"></td>
         <td class="ev-th" style="white-space:nowrap;">순내부체적 :</td>
-        <td colspan="3"><input data-field="ev_room_vol" class="ev-inp" type="text" value="\${E(v('ev_room_vol'))}"></td>
+        <td><input data-field="ev_room_vol" class="ev-inp" type="text" value="\${E(v('ev_room_vol'))}"></td>
       </tr>
       <!-- 측정실 온도 조정방법 / 연료가열장치 / 측정실 모델 -->
       <tr>
         <td class="ev-th" style="white-space:nowrap;">측정실 온도 조정방법 :</td>
-        <td colspan="3"><input data-field="ev_temp_method" class="ev-inp" type="text" value="\${E(v('ev_temp_method'))}"></td>
+        <td><input data-field="ev_temp_method" class="ev-inp" type="text" value="\${E(v('ev_temp_method'))}"></td>
         <td class="ev-th" style="white-space:nowrap;">연료가열장치 :</td>
         <td><input data-field="ev_fuel_heater" class="ev-inp" type="text" value="\${E(v('ev_fuel_heater'))}"></td>
         <td class="ev-th" style="white-space:nowrap;">측정실 모델 :</td>
@@ -3660,7 +3658,7 @@ if (formType==='detail_plan') return (
       <!-- 분석장비 / HC 고정 방법 / 모델 -->
       <tr>
         <td class="ev-th" style="white-space:nowrap;">분석장비 :</td>
-        <td colspan="3"><input data-field="ev_analyzer" class="ev-inp" type="text" value="\${E(v('ev_analyzer'))}"></td>
+        <td><input data-field="ev_analyzer" class="ev-inp" type="text" value="\${E(v('ev_analyzer'))}"></td>
         <td class="ev-th" style="white-space:nowrap;">HC 고정 방법 :</td>
         <td><input data-field="ev_hc_method" class="ev-inp" type="text" value="\${E(v('ev_hc_method'))}"></td>
         <td class="ev-th" style="white-space:nowrap;">모&nbsp;&nbsp;&nbsp;델 :</td>
@@ -3671,13 +3669,13 @@ if (formType==='detail_plan') return (
         <td class="ev-th" rowspan="2" style="text-align:center; vertical-align:middle;">활성탄<br>채집트랙</td>
         <td class="ev-th" style="white-space:nowrap;">용기규격 및 재질 :</td>
         <td colspan="2"><input data-field="ev_can_spec" class="ev-inp" type="text" value="\${E(v('ev_can_spec'))}"></td>
-        <td class="ev-th" style="white-space:nowrap;">보조채집장치의 규격 및 재질 :</td>
-        <td colspan="3"><input data-field="ev_aux_spec" class="ev-inp" type="text" value="\${E(v('ev_aux_spec'))}"></td>
+        <td class="ev-th" style="white-space:nowrap; font-size:7.5pt;">보조채집장치의 규격 및 재질 :</td>
+        <td><input data-field="ev_aux_spec" class="ev-inp" type="text" value="\${E(v('ev_aux_spec'))}"></td>
       </tr>
       <!-- 활성탄 채집트랙 - 2행: 채집용기 무게 / 시험후 무게 / 손무게 -->
       <tr>
         <td class="ev-th" style="white-space:nowrap;">채집용기 무게 :</td>
-        <td colspan="2"><input data-field="ev_can_wt_before" class="ev-inp" type="text" placeholder="g" value="\${E(v('ev_can_wt_before'))}"></td>
+        <td><input data-field="ev_can_wt_before" class="ev-inp" type="text" placeholder="g" value="\${E(v('ev_can_wt_before'))}"></td>
         <td class="ev-th" style="white-space:nowrap;">시험후 무게 :</td>
         <td><input data-field="ev_can_wt_after" class="ev-inp" type="text" placeholder="g" value="\${E(v('ev_can_wt_after'))}"></td>
         <td class="ev-th" style="white-space:nowrap;">손무게 :</td>

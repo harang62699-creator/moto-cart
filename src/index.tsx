5159994a84a870d164fc31f737dd0155ff17ad41
genspark_dev <genspark_dev@genspark.ai>
2026-04-27 05:09:38 +0000

    fix: OBD 시험결과 표 헤더 3행 구조로 통합 - 장치명/오작동재현조건을 CO·NOx·HC와 동일한 행에 배치

--- src/index.tsx
+++ src/index.tsx
@@ -3652,38 +3652,27 @@ if (formType==='detail_plan') return (
     <thead>
       <!-- 1행: 대분류 -->
       <tr>
-        <th class="obd-result-th-top" colspan="2" rowspan="3" style="vertical-align:middle;">시험대상<br>감시장치</th>
+        <th class="obd-result-th-top" colspan="2" rowspan="2" style="vertical-align:middle;">시험대상 감시장치</th>
         <th class="obd-result-th-top" colspan="4">시험결과</th>
         <th class="obd-result-th-top" colspan="4">결과판정</th>
       </tr>
       <!-- 2행: 중분류 -->
       <tr>
-        <th class="obd-result-th-mid" colspan="3">CVS-75모드 결과 (g/km)</th>
+        <th class="obd-result-th-mid" colspan="3">CVS-75모드<br>결과 (g/km)</th>
         <th class="obd-result-th-mid" rowspan="2" style="vertical-align:middle; font-size:7pt;">오작동<br>표시등<br>점등여부</th>
-        <th class="obd-result-th-mid" colspan="3">오작동 판단 기준 (g/km)</th>
+        <th class="obd-result-th-mid" colspan="3">오작동 판단 기준<br>(g/km)</th>
         <th class="obd-result-th-mid" rowspan="2" style="vertical-align:middle; font-size:7pt;">감시장치<br>적부판정</th>
       </tr>
-      <!-- 3행: 소분류 -->
+      <!-- 3행: 장치명/재현조건 + CO·NOx·HC 소분류 — 모두 같은 행 -->
       <tr>
+        <th class="obd-result-th-mid" style="vertical-align:middle;">장치명</th>
+        <th class="obd-result-th-mid" style="vertical-align:middle;">오작동<br>재현조건</th>
         <th class="obd-result-th-mid">CO</th>
         <th class="obd-result-th-mid">NOx</th>
         <th class="obd-result-th-mid">HC</th>
         <th class="obd-result-th-mid">CO</th>
         <th class="obd-result-th-mid">NOx</th>
         <th class="obd-result-th-mid">HC</th>
-      </tr>
-      <!-- 4행: 장치명/재현조건 라벨 -->
-      <tr>
-        <th class="obd-result-th-mid">장치명</th>
-        <th class="obd-result-th-mid">오작동<br>재현조건</th>
-        <th class="obd-result-th-mid"></th>
-        <th class="obd-result-th-mid"></th>
-        <th class="obd-result-th-mid"></th>
-        <th class="obd-result-th-mid"></th>
-        <th class="obd-result-th-mid"></th>
-        <th class="obd-result-th-mid"></th>
-        <th class="obd-result-th-mid"></th>
-        <th class="obd-result-th-mid"></th>
       </tr>
     </thead>
     <tbody>
